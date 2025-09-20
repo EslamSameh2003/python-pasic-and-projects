2cd534210617c4c011429d7dae3b15be0fcd80ab
EslamSameh2003 <eslamsameh0081@gmail.com>
2025-09-20 16:20:18 +0300

    delete

--- Doctor.py
+++
@@ -1,7 +0,0 @@
-class doctor:
-    def stydied_yers(self):
-        print("i stydied 7 years")
-    def work_where(self):
-        print("in hospital")
-    def paid_who(self):
-        print(" government")
--- familydoctor.py
+++
@@ -1,7 +0,0 @@
-from Doctor import doctor
-
-class family_doctor(doctor):
-    def works(self):
-        print("in home")
-    def paid_who(self):
-        print("from patient")
